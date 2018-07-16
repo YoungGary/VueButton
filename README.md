5b0a5c5bff465d2ffb9ac5f884c2fa3f3fb6d388
YoungGary <599045318@qq.com>
2018-07-16 14:18:22 +0800

    button css

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # VueButton
 vue button 轮子 从0到1
+步骤
+1 npm init  创建项目
+2 引入包
+
 
---
+++ button.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Gary on 2018/7/16.
+ */
+
+Vue.component('g-button',{
+    template:`
+        <button class="g-button">提交</button>
+     `
+})
+
+
+
