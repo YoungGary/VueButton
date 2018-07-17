fa17561ee0495bfabedf97670871352abe61a643
YoungGary <599045318@qq.com>
2018-07-17 11:21:02 +0800

    parcel 构建项目

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lim on 2018/7/17.
+ */
+
+
+
+import  Vue from 'vue'
+import  button from './button'
+
+Vue.component('g-button',button)
+new Vue({
+    el:'#app',
+
+})
---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="g-button">提交</button>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+    .g-button{
+        font-size:var(--font_size);
+        height:var(--button-height);
+        padding:0 1em;
+        border-radius: var(--border-radius);
+        border:1px solid var(--border-color);
+        background: var(--button-bg);
+    }
+    .g-button:hover{
+        border-color: var(--border-color-hover);
+    }
+    .g-button:active{
+        background: var(--button-active-bg);
+    }
+    .g-button:focus{
+        outline: none;
+    }
+</style>
