ea90a2453c9106a6f7801290835f5219884fa74f
YoungGary <599045318@qq.com>
2018-07-23 09:14:55 +0800

    loading 改变状态

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@
 
 import  Vue from 'vue'
 import  button from './button'
+import icon from './iconj'
 
 Vue.component('g-button',button)
+Vue.component('g-icon',icon)
 new Vue({
     el:'#app',
-
+    data:{
+        loading:false
+    }
 })
+
+
